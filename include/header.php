cc6502cfcd552ac3fa4b7eff51ddd7358f89323e
Heberth Matos <heberthhm.hm@outlook.com>
2024-10-16 20:13:09 -0300

    atualização

--- include/header.php
+++ include/header.php
@@ -1,8 +1,9 @@
 <header>
-    <div class="logo-nome"> <img src="/img/logo.png" alt="Logo do Advogado" class="logo">
-        <span class="nome">NOME ESCRITORIO</span>
+    <div class="logo-nome">
+        <img src="/img/logo.png" alt="Logo do Advogado" class="logo">
+        <span class="nome">NOME ESCRITÓRIO</span>
     </div>
-    <nav class="nav">
+    <nav class="nav" id="nav">
         <a href="#home">Home</a>
         <a href="#areas">Áreas de Atuação</a>
         <a href="#sobre-nos">Sobre Nós</a>
